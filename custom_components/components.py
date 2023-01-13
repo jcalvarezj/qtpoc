db4dd636358a817ba9ee16d6b4baa486293ad62d
jcalvarezj <jcalvarezj@gmail.com>
2023-01-12 23:56:47 -0500

    Implement inserts

--- custom_components/components.py
+++ custom_components/components.py
@@ -1,6 +1,6 @@
 # Custom components with Qt Designer base
 
-from PySide6.QtWidgets import QMainWindow, QDialog, QMessageBox
+from PySide6.QtWidgets import QMainWindow, QDialog, QErrorMessage
 from bson.json_util import dumps
 from custom_components.ui_main import Ui_MainWindow
 from custom_components.ui_dialog import Ui_Dialog
@@ -13,7 +13,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.db_name = db_name
         self.setupUi(self)
 
-        self.btnQuery.pressed.connect(self.query_action)
+        self.btn_query.pressed.connect(self.query_action)
+        self.btn_insert.pressed.connect(self.insert_action)
 
     def query_action(self):
         self.lvwData.clear()
@@ -23,17 +24,31 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         search_field = ""
 
-        if self.rbtId.isChecked():
+        if self.rbt_id.isChecked():
             search_field = "cedula"
-        if self.rbtFirstName.isChecked():
+        if self.rbt_first_name.isChecked():
             search_field = "nombre"
-        if self.rbtLastName.isChecked():
+        if self.rbt_last_name.isChecked():
             search_field = "apellido"
 
         found = personas_collection.find({search_field: {"$regex": self.txtSearch.text(), "$options": "i"}})
 
         for person in found:
             self.lvwData.addItem(f"{person['nombre']} {person['apellido']} ({person['cedula']})")
+
+    def insert_action(self):
+        personas_collection = self.mongo_client[self.db_name].personas
+
+        try:
+            personas_collection.insert_one({
+                "nombre": self.txt_first_name.text(),
+                "apellido": self.txt_last_name.text(),
+                "cedula": self.txt_id.text()
+            })
+        except Exception as e:
+            error_dialog = QErrorMessage()
+            error_dialog.showMessage('Oh no! - ' + e)
+
 
 
 class Dialog(QDialog, Ui_Dialog):
--- custom_components/ui_main.py
+++ custom_components/ui_main.py
@@ -10,9 +10,8 @@
 
 from PySide6.QtCore import QCoreApplication, QMetaObject, QRect
 from PySide6.QtWidgets import (QButtonGroup, QGridLayout, QGroupBox, QLabel,
-                               QLineEdit, QListWidget, QMenuBar, QPushButton,
-                               QRadioButton, QStatusBar, QVBoxLayout, QWidget)
-
+    QLineEdit, QListWidget, QMenuBar, QPushButton, QRadioButton, QStatusBar,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -40,10 +39,10 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addWidget(self.lvwData)
 
-        self.btnQuery = QPushButton(self.verticalLayoutWidget)
-        self.btnQuery.setObjectName(u"btnQuery")
+        self.btn_query = QPushButton(self.verticalLayoutWidget)
+        self.btn_query.setObjectName(u"btn_query")
 
-        self.verticalLayout.addWidget(self.btnQuery)
+        self.verticalLayout.addWidget(self.btn_query)
 
         self.verticalLayoutWidget_3 = QWidget(self.groupBox)
         self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
@@ -51,65 +50,75 @@ class Ui_MainWindow(object):
         self.verticalLayout_3 = QVBoxLayout(self.verticalLayoutWidget_3)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.rbtFirstName = QRadioButton(self.verticalLayoutWidget_3)
+        self.rbt_first_name = QRadioButton(self.verticalLayoutWidget_3)
         self.buttonGroup = QButtonGroup(MainWindow)
         self.buttonGroup.setObjectName(u"buttonGroup")
-        self.buttonGroup.addButton(self.rbtFirstName)
-        self.rbtFirstName.setObjectName(u"rbtFirstName")
-        self.rbtFirstName.setChecked(True)
+        self.buttonGroup.addButton(self.rbt_first_name)
+        self.rbt_first_name.setObjectName(u"rbt_first_name")
+        self.rbt_first_name.setChecked(True)
 
-        self.verticalLayout_3.addWidget(self.rbtFirstName)
+        self.verticalLayout_3.addWidget(self.rbt_first_name)
 
-        self.rbtLastName = QRadioButton(self.verticalLayoutWidget_3)
-        self.buttonGroup.addButton(self.rbtLastName)
-        self.rbtLastName.setObjectName(u"rbtLastName")
+        self.rbt_last_name = QRadioButton(self.verticalLayoutWidget_3)
+        self.buttonGroup.addButton(self.rbt_last_name)
+        self.rbt_last_name.setObjectName(u"rbt_last_name")
 
-        self.verticalLayout_3.addWidget(self.rbtLastName)
+        self.verticalLayout_3.addWidget(self.rbt_last_name)
 
-        self.rbtId = QRadioButton(self.verticalLayoutWidget_3)
-        self.buttonGroup.addButton(self.rbtId)
-        self.rbtId.setObjectName(u"rbtId")
+        self.rbt_id = QRadioButton(self.verticalLayoutWidget_3)
+        self.buttonGroup.addButton(self.rbt_id)
+        self.rbt_id.setObjectName(u"rbt_id")
 
-        self.verticalLayout_3.addWidget(self.rbtId)
+        self.verticalLayout_3.addWidget(self.rbt_id)
 
         self.groupBox_2 = QGroupBox(self.centralwidget)
         self.groupBox_2.setObjectName(u"groupBox_2")
-        self.groupBox_2.setGeometry(QRect(480, 0, 441, 131))
+        self.groupBox_2.setGeometry(QRect(480, 0, 441, 191))
         self.verticalLayoutWidget_2 = QWidget(self.groupBox_2)
         self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
-        self.verticalLayoutWidget_2.setGeometry(QRect(50, 30, 381, 79))
+        self.verticalLayoutWidget_2.setGeometry(QRect(20, 30, 411, 141))
         self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget_2)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
         self.gridLayout_3 = QGridLayout()
         self.gridLayout_3.setObjectName(u"gridLayout_3")
-        self.txtFirstName = QLineEdit(self.verticalLayoutWidget_2)
-        self.txtFirstName.setObjectName(u"txtFirstName")
+        self.label_2 = QLabel(self.verticalLayoutWidget_2)
+        self.label_2.setObjectName(u"label_2")
 
-        self.gridLayout_3.addWidget(self.txtFirstName, 0, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.label_2, 2, 1, 1, 1)
 
         self.label = QLabel(self.verticalLayoutWidget_2)
         self.label.setObjectName(u"label")
 
-        self.gridLayout_3.addWidget(self.label, 0, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.label, 1, 1, 1, 1)
 
-        self.txtLastName = QLineEdit(self.verticalLayoutWidget_2)
-        self.txtLastName.setObjectName(u"txtLastName")
+        self.label_3 = QLabel(self.verticalLayoutWidget_2)
+        self.label_3.setObjectName(u"label_3")
 
-        self.gridLayout_3.addWidget(self.txtLastName, 1, 2, 1, 1)
+        self.gridLayout_3.addWidget(self.label_3, 0, 1, 1, 1)
 
-        self.label_2 = QLabel(self.verticalLayoutWidget_2)
-        self.label_2.setObjectName(u"label_2")
+        self.txt_id = QLineEdit(self.verticalLayoutWidget_2)
+        self.txt_id.setObjectName(u"txt_id")
 
-        self.gridLayout_3.addWidget(self.label_2, 1, 1, 1, 1)
+        self.gridLayout_3.addWidget(self.txt_id, 0, 2, 1, 1)
+
+        self.txt_first_name = QLineEdit(self.verticalLayoutWidget_2)
+        self.txt_first_name.setObjectName(u"txt_first_name")
+
+        self.gridLayout_3.addWidget(self.txt_first_name, 1, 2, 1, 1)
+
+        self.txt_last_name = QLineEdit(self.verticalLayoutWidget_2)
+        self.txt_last_name.setObjectName(u"txt_last_name")
+
+        self.gridLayout_3.addWidget(self.txt_last_name, 2, 2, 1, 1)
 
 
         self.verticalLayout_2.addLayout(self.gridLayout_3)
 
-        self.btnInsert = QPushButton(self.verticalLayoutWidget_2)
-        self.btnInsert.setObjectName(u"btnInsert")
+        self.btn_insert = QPushButton(self.verticalLayoutWidget_2)
+        self.btn_insert.setObjectName(u"btn_insert")
 
-        self.verticalLayout_2.addWidget(self.btnInsert)
+        self.verticalLayout_2.addWidget(self.btn_insert)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
@@ -128,13 +137,14 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Query", None))
-        self.btnQuery.setText(QCoreApplication.translate("MainWindow", u"Query", None))
-        self.rbtFirstName.setText(QCoreApplication.translate("MainWindow", u"First Name", None))
-        self.rbtLastName.setText(QCoreApplication.translate("MainWindow", u"Last Name", None))
-        self.rbtId.setText(QCoreApplication.translate("MainWindow", u"Document Number", None))
+        self.btn_query.setText(QCoreApplication.translate("MainWindow", u"Query", None))
+        self.rbt_first_name.setText(QCoreApplication.translate("MainWindow", u"First Name", None))
+        self.rbt_last_name.setText(QCoreApplication.translate("MainWindow", u"Last Name", None))
+        self.rbt_id.setText(QCoreApplication.translate("MainWindow", u"Document Number", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Insert", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"First Name", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Last Name", None))
-        self.btnInsert.setText(QCoreApplication.translate("MainWindow", u"Insert", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"First Name", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Doc. Num.", None))
+        self.btn_insert.setText(QCoreApplication.translate("MainWindow", u"Insert", None))
     # retranslateUi
 
